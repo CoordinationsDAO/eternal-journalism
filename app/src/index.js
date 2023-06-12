4b3c6092d1d1d40f847afddf5bb15dff9565cebe
Daniella Mesquita <mesquitadaniella@proton.me>
2023-06-12 11:59:20 -0300

    Implement ChatGPT's guidance

---
+++ app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EternalJournalismApp from './EternalJournalismApp';
+
+ReactDOM.render(<EternalJournalismApp />, document.getElementById('root'));
+
